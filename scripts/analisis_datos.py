b372bd576f6334f95581c8e638b817b0746559d5
Nicolas <nicolascontacto20@gmail.com>
2026-05-25 22:17:31 +0000

    PO-06: Agregar scripts de análisis de datos

---
+++ scripts/analisis_datos.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+ 
+def temperatura_promedio(temp):
+  add = 0
+  for i in range(len(temp)):
+    add += temp[i]
+  return add/i
+
+def temperaturaMaximaYMinima(temp):
+  min = 1000
+  max = 0
+  for i in range(len(temp)):
+    if temp[i] < min:
+      min = temp[i]
+  for j in range(len(temp)):
+    if temp[j] > max:
+      max = temp[j]
+
+  return min, max
+
+def promedioDePrecipitaciones(precip):
+  prom = 0
+  for i in range(len(precip)):
+    prom += precip[i]
+  return prom/i
